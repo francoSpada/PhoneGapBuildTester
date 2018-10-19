e4f6c48a6a8bfeb458210e4be6c6cf2ac603d18e
francospadavecchia <francospadavecchia@cnea.gov.ar>
2018-10-19 12:19:56 -0300

    Draw test3

--- www/js/geom.js
+++ www/js/geom.js
@@ -19,5 +19,5 @@ class Punto{
 
 
 function distancia(p1,p2){
-    return Math.sqrt(Math.pow( ( p1.x()-p2.x() ),2)+Math.pow( ( p1.y()-p2.y() ),2))
+    return Math.sqrt(Math.pow( ( p1.x-p2.x ),2)+Math.pow( ( p1.y-p2.y ),2))
 }
