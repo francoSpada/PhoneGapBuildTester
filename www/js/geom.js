75a4902abc32c90f5d263da36c4a4e1f7155c33a
francoSpada <francospadap613@gmail.com>
2018-10-21 15:15:22 -0300

    inicializada grilla

--- www/js/geom.js
+++ www/js/geom.js
@@ -1,22 +1,5 @@
 
 
-class Punto{
-    
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
-
-    x(){
-        return this.x;
-    }
-
-    y(){
-        return this.y
-    }
-
-}
-
 
 function distancia(p1,p2){
     return Math.sqrt(Math.pow( ( p1.x-p2.x ),2)+Math.pow( ( p1.y-p2.y ),2))
---
+++ www/js/objetos_dibujo.js
@@ -0,0 +1,45 @@
+class Punto{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    x(){
+        return this.x;
+    }
+    y(){
+        return this.y
+    }
+}
+
+class Grilla{
+    constructor(tam_grilla){
+        this.tam_grilla = tam_grilla;
+        //this.fillStyle = "rgba(0, 0, 200, 0.2)";
+        this.lineWidth = 0.4;
+		this.strokeStyle = "rgba(240, 240, 240, 1)";
+    }
+
+    dibujar(wdw){
+        ctx = wdw.ctx;
+        ctx.lineWidth = this.lineWidth;
+		ctx.strokeStyle = this.strokeStyle;
+        //verticales
+        for(var x = this.tam_grilla/2; x < wdw.w; x += this.tam_grilla){
+            ctx.moveTo(x,0);
+            ctx.lineTo(x,wdw.h);
+            ctx.stroke();
+        }
+        for(var y = this.tam_grilla/2; y < wdw.h; y += this.tam_grilla){
+            ctx.moveTo(0,y);
+            ctx.lineTo(wdw.w,y);
+            ctx.stroke();
+        }
+    }
+
+    punto_mas_cercano(p){
+        var x = p.x - p.x%this.tam_grilla + this.tam_grilla/2;
+        var y = p.y - p.y%this.tam_grilla + this.tam_grilla/2;
+        return(new Punto(x,y))
+    }
+
+}
