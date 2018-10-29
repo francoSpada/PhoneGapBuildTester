5e3198acebdb0d6a6fefe1989bf21bd661a7c13f
francoSpada <francospadap613@gmail.com>
2018-10-29 19:51:57 -0300

    Figuras movibles

--- www/js/geom.js
+++ www/js/geom.js
@@ -3,17 +3,6 @@ function distancia(p1,p2){
     return Math.sqrt(Math.pow( ( p1.x-p2.x ),2)+Math.pow( ( p1.y-p2.y ),2))
 }
 
-
-/*
-def anguloVV(x0,y0,x1,y1):
-	m1 = pow(x0,2)+pow(y0,2)
-	m2 = pow(x1,2)+pow(y1,2)
-	m = math.sqrt(m1*m2)
-	if m1 == 0 or m2 == 0:
-		return 0
-	pe = x0*x1 + y0*y1
-	return math.degrees(math.acos(pe/m))
-*/
 function angulo_entre_vectores(v1,v2){
     var m1 = Math.pow(v1.x,2)+Math.pow(v1.y,2);
 	var m2 = Math.pow(v2.x,2)+Math.pow(v2.y,2);
@@ -23,4 +12,47 @@ function angulo_entre_vectores(v1,v2){
     }
     var pe = v1.x*v2.x + v1.y*v2.y;
     return(Math.acos(pe/m)*180/Math.PI);
+}
+
+function interseccion_recta_con_horizontal(pr,p1,p2){
+    //pr punto de referencia
+    //p1 y p2 puntos de la recta
+    if( (p1.y > pr.y && p2.y > pr.y) || (p1.y < pr.y && p2.y < pr.y) ){
+        console.log("a");
+        return false;
+    }else if(p1.x > pr.x && p2.x > pr.x){
+        return true;
+    }else if(p1.x < pr.x && p2.x < pr.x){
+        console.log("b");
+        return false;
+    }else{
+        if(p1.x < p2.x){
+            var pi = p1;
+            var pf = p2;
+        }else{
+            var pi = p2;
+            var pf = p1;
+        }
+        var arecta = Math.abs((pf.y - pi.y)/(pf.x-pi.x));
+        var aref = Math.abs((pf.y - pr.y)/(pf.x-pr.x));
+        if(aref < arecta){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+function punto_dentro_de_vertices(p,vertices){
+    var cuenta_intersecciones = 0;
+    for(var v = 1; v < vertices.length; v++){
+        if(interseccion_recta_con_horizontal(p,vertices[v-1],vertices[v])){
+            cuenta_intersecciones += 1;
+        }
+    }
+    if(cuenta_intersecciones % 2 == 0){
+        return false;
+    }else{
+        return true;
+    }
 }
--- www/js/objetos_dibujo.js
+++ www/js/objetos_dibujo.js
@@ -22,11 +22,73 @@ class Punto{
         var norm = Math.sqrt(this.x*this.x+this.y*this.y);
         return(new Punto(new_norm*this.x/norm, new_norm*this.y/norm));
     }
+
+    esIgualA(p){
+        if(p.x == this.x && p.y == this.y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
 
 Punto.prototype.toString = function()
 {
     return "x: " + this.x.toFixed(2) + ", y: " + this.y.toFixed(2) ;
+}
+
+class Figura_Grilla{
+    constructor(vert){
+        /*this.punto_central = new Punto(0,0);
+        for(var v in vert){
+            this.punto_central = punto_central.pointPlus(vert[v]);
+        }
+        this.vertices = [];
+        for(var v in vert){
+            this.vertices.push(this.punto_central.vectorTo(vert[v]));
+        }*/
+        this.vertices = vert;
+        this.seleccionada = false;
+    }
+
+    punto_es_interior(punto){
+        return punto_dentro_de_vertices(punto,this.vertices);
+    }
+
+    desplazar(vector){
+        for(var v in this.vertices){
+            this.vertices[v] = new Punto(this.vertices[v].x + vector.x,
+                                        this.vertices[v].y + vector.y );
+        }
+    }
+
+    dibujar(ctx){
+        if(this.vertices.length >= 1){
+            ctx.beginPath();
+            ctx.moveTo(this.vertices[0].x,this.vertices[0].y);
+            for(var i=1;i<this.vertices.length; i++){
+                ctx.lineTo(this.vertices[i].x,this.vertices[i].y);
+            }
+            ctx.closePath()
+            ctx.fillStyle = "rgba(0, 0, 100, 0.2)";
+            ctx.fill();
+            ctx.lineWidth = 0.8;
+            ctx.strokeStyle = "rgba(0, 0, 75, 1)";
+            ctx.stroke();
+            for(var i=0;i<this.vertices.length; i++){
+                ctx.beginPath();
+                ctx.moveTo(this.vertices[i].x-5,this.vertices[i].y-5);
+                ctx.lineTo(this.vertices[i].x-5,this.vertices[i].y+5);
+                ctx.lineTo(this.vertices[i].x+5,this.vertices[i].y+5);
+                ctx.lineTo(this.vertices[i].x+5,this.vertices[i].y-5);
+                ctx.closePath();
+                ctx.stroke();
+            }
+        }
+
+    }
 }
 
 class Grilla{
